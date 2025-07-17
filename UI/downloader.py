153ffcd30c951f768b8e0733502bc5be09d1e28c
Marco Méndez <37358148+MarcoAntonioMendez@users.noreply.github.com>
2025-07-16 22:43:45 -0600

    Main window frame created
    The main program window frame gets created, widht and height is set and the window is placed in the middle of the computer screen.

--- UI/downloader.py
+++ UI/downloader.py
@@ -1,7 +1,25 @@
+import tkinter as tk
+
+
+WINDOW_WIDTH = 960
+WINDOW_HEIGHT = 540
+
 class Downloader:
     def __init__(self):
-        print()
+        # Initializing the root to contain the main frame of the GUI application
+        self.__root = tk.Tk()
 
 
     def start(self):
-        print('Software starts')
+        # Calculating some coordinates to center the window frame in the middle of computer screen
+        self.__root.minsize(WINDOW_WIDTH,WINDOW_HEIGHT)
+        self.__root.maxsize(WINDOW_WIDTH,WINDOW_HEIGHT)
+        screen_width = self.__root.winfo_screenwidth()
+        screen_height = self.__root.winfo_screenheight()
+        x_pos = int((screen_width - WINDOW_WIDTH)/2)
+        y_pos = int((screen_height - WINDOW_HEIGHT)/2)
+        self.__root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{x_pos}+{y_pos}")
+
+
+        # Starting the windows
+        self.__root.mainloop()
