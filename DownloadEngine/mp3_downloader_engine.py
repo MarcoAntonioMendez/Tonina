c8bca9eaa1cab42be5b0f8635adbece170f662d6
Marco Méndez <37358148+MarcoAntonioMendez@users.noreply.github.com>
2025-08-03 01:11:49 -0600

    Implemented user feedback when downloading a song on download engine
    Now the software informs the user in the graphic interface in which state the download is at.

--- DownloadEngine/mp3_downloader_engine.py
+++ DownloadEngine/mp3_downloader_engine.py
@@ -1,10 +1,12 @@
 import yt_dlp
 import os
 import logging
+import customtkinter as ctk
 from pathlib import Path
 
 # Constants
 BETA_KEYBOARD = "_beta"
+RETURN_TO_MAIN_WINDOW_BUTTON_ENABLED_BACKGROUND_COLOR = "#59239a"
 
 
 class Mp3DownloaderEngine:
@@ -13,7 +15,8 @@ class Mp3DownloaderEngine:
 
     def download_song(self, song_title: str, artist_name: str, album_name: str,\
                     track_position_in_album: str, song_genre: str, youtube_url: str,\
-                    album_cover_image: str):
+                    album_cover_image: str, return_to_main_window_button,\
+                    progress_bar, download_status_label):
         self.__song_title = song_title
         self.__artist_name = artist_name
         self.__album_name = album_name
@@ -21,10 +24,14 @@ class Mp3DownloaderEngine:
         self.__song_genre = song_genre
         self.__youtube_url = youtube_url
         self.__album_cover_image = album_cover_image
+        self.__return_to_main_window_button = return_to_main_window_button
+        self.__progress_bar = progress_bar
+        self.__download_status_label = download_status_label
 
 
         #--------------------- Making the actual download ------------------------------
         logging.warning("Starting to download: " + self.__song_title)
+        self.__progress_bar.step()
 
         # Setting the filename
         safe_song_title_for_filename = self.clear_string_from_forbidden_chars_for_file_names(self.__song_title)
@@ -33,6 +40,8 @@ class Mp3DownloaderEngine:
 
         #Changing to the default download directory
         os.chdir(str(Path.home())+"/Downloads")
+        self.__progress_bar.step()
+        self.__download_status_label.configure(text="Extrayendo audio...")
 
         # Setting up YoutubeDLP options
         ydl_opts = {
@@ -44,10 +53,12 @@ class Mp3DownloaderEngine:
             }],
             'outtmpl': filename,
         }
+        self.__progress_bar.step()
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.extract_info(self.__youtube_url)
 
         logging.warning("The raw download of: " + self.__song_title + " has been finished (Metadata has not been set yet).")
+        self.__progress_bar.step()
 
         # Now that the raw download of the song has finished, it's time to set the metadata.
         logging.warning("Setting up the metadata for: " + filename)
@@ -55,6 +66,7 @@ class Mp3DownloaderEngine:
 
         # Creating command
         command_to_add_metadata = ""
+        self.__progress_bar.step()
 
         # Adding the initial part for using ffmpeg
         command_to_add_metadata += "ffmpeg -i "
@@ -84,13 +96,24 @@ class Mp3DownloaderEngine:
 
         # Adding the quality and final output filename
         command_to_add_metadata += " -b:a 320k " + filename.replace(BETA_KEYBOARD, "") + ".mp3"
+        self.__progress_bar.step()
 
 
         # Executing the metadata command
+        self.__download_status_label.configure(text="Estableciendo metadata...")
         os.system(command_to_add_metadata)
+        self.__progress_bar.step()
 
         # Removing leftover files
         os.remove(filename+".mp3")
+        self.__progress_bar.step()
+        self.__download_status_label.configure(\
+            text="DESCARGA FINALIZADA! El Archivo ha sido guardado en la carpeta de descargas :D.")
+
+
+        # Enabling the return to the main window button
+        self.__return_to_main_window_button.configure(state=ctk.NORMAL,\
+            fg_color=RETURN_TO_MAIN_WINDOW_BUTTON_ENABLED_BACKGROUND_COLOR)
 
 
     def clear_string_from_forbidden_chars_for_file_names(self, string_to_clean):
